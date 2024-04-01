113e8cb6a4f0c7cf1995202c31a30c22b992a30c
dunglv <dunglv202@gmail.com>
2024-04-01 21:34:28 +0700

    config services

--- food-service/src/main/java/com/example/food/FoodServiceApplication.java
+++ food-service/src/main/java/com/example/food/FoodServiceApplication.java
@@ -2,8 +2,13 @@ package com.example.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FoodServiceApplication {
 
 	public static void main(String[] args) {
