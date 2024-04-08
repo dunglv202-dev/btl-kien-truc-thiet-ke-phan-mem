22e106cd3fce707ff9a7ec143ae47e3bd453a967
dunglv <dunglv@bsscommerce.com>
2024-04-09 00:36:25 +0700

    reinitialize services

---
+++ catalog-service/src/test/java/vn/edu/ptit/kttk/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ptit.kttk.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ customer-service/src/test/java/vn/edu/ptit/kttk/customer/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ptit.kttk.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
